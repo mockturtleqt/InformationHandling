722ad0f7687d17275dbca745148a8e6b5011121e
mockturtleqt <galia.semashko@gmail.com>
2016-11-14 22:20:35 +0300

    Comment added

--- src/com/epam/fourth/interpreter/TerminalExpressionMinus.java
+++ src/com/epam/fourth/interpreter/TerminalExpressionMinus.java
@@ -4,7 +4,7 @@ class TerminalExpressionMinus extends AbstractMathExpression {
 
     public void interpret(Context context) {
         Double secondValue = context.popValue();
-        Double firstValue = 0.0;
+        Double firstValue = 0.0; //if we need to interpret a single negative number ((-5) for example)
         if (!context.isEmpty()) {
             firstValue = context.popValue();
         }
